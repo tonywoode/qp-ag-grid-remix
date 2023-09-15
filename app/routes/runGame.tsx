68d8a58924024c1773ca0d2533510e275590f0eb
tony woode <tonywoodecode@gmail.com>
2023-09-15 10:22:04 +0100

    swapping node-7z to node-7z-archive...
    node-7z-archive is an esm module, remix does not like this module at
    present: https://github.com/remix-run/remix/discussions/2594
    none of those ideas worked, dynamic import did work!

--- app/routes/runGame.tsx
+++ app/routes/runGame.tsx
@@ -1,34 +1,47 @@
 import { ActionArgs, json } from '@remix-run/node'
-
-const Seven = require('node-7z')
+//import { extractArchive } from 'node-7z-archive'
 
 export async function action({ request }: ActionArgs) {
   const body = await request.json()
 
   console.log(body)
-  const runGameIfItsGotDefaultGoodmerge = (path, defaultGoodMerge) => {
+  const runGameIfItsGotDefaultGoodmerge = async (path, defaultGoodMerge) => {
     console.log(path, defaultGoodMerge)
 
     //hardcoode to demonstrate extracting defaultGoodmerge
     const archivePath = '/Volumes/Untitled/Games/Sega Games/Genesis Games/GoodGEN_3_GM/Atomic Robo-Kid.7z'
     const filePathInsideArchive = 'Atomic Robo-Kid (U) [c][!].gen'
-    const outputDirectory = '~/../temp'
-    const options = {
-      recursive: true, // extract subdirectories as well
-      $cherryPick: filePathInsideArchive // Specify the file you want to extract
-    }
-    Seven.extractFull(archivePath, outputDirectory, options, (err, files) => {
-      if (err) {
-        console.error(`Error extracting archive: ${err}`)
-        return
-      }
+    const outputDirectory = '/Users/twoode/CODE/Scripts/qp-ag-grid/qp-ag-grid-remix/qp-ag-grid-remix/temp'
 
-      console.log('Extraction complete')
-      // Now you can pass the extracted file to another program
-
-      // Clean up the temporary extracted file if needed
-    })
+    //its an ESM module, use dynamic import inline here, don't try adding it to the serverDependenciesToBundle in remix.config.js, that won't work
+    const node7z = await import('node-7z-archive')
+    //const node7z = moduleA.default()
+    //console.log(node7z)
+    const { extractArchive } = node7z
+    await extractArchive(archivePath, outputDirectory)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
+  //     const options = {
+  //       recursive: true, // extract subdirectories as well
+  //       $cherryPick: filePathInsideArchive // Specify the file you want to extract
+  //     }
+  //     Seven.extractFull(archivePath, outputDirectory, options, (err, files) => {
+  //       if (err) {
+  //         console.error(`Error extracting archive: ${err}`)
+  //         return
+  //       }
+
+  //       console.log('Extraction complete')
+  //       // Now you can pass the extracted file to another program
+
+  //       // Clean up the temporary extracted file if needed
+  //     })
+  //   }
   runGameIfItsGotDefaultGoodmerge()
   console.log('yep you hit me')
   return null
