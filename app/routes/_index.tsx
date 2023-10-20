d310185ea440dfd2786e343a0c98111fad250236
tony woode <tonywoodecode@gmail.com>
2023-10-20 20:13:21 +0100

    try arborist as treeview

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import AgThemeAlpineStyles from "ag-grid-community/styles/ag-theme-alpine.css";
 import { useLoaderData } from "@remix-run/react";
 import { romdata } from '~/../outputs/romdata.json' //note destructuring
 import { CellClickedEvent } from 'ag-grid-community'
+import { Tree } from 'react-arborist'
 
 //configure and export logging per-domain feature
 import { createFeatureLogger } from '~/utils/featureLogger'
@@ -43,13 +44,39 @@ export async function loader() {
   return json({ romdata })
 }
 
+const treeData = [
+  {
+    id: '1',
+    name: 'public',
+    children: [{ id: 'c1-1', name: 'index.html' }]
+  },
+  {
+    id: '2',
+    name: 'src',
+    children: [
+      { id: 'c2-1', name: 'App.js' },
+      { id: 'c2-2', name: 'index.js' },
+      { id: 'c2-3', name: 'styles.css' }
+    ]
+  },
+  { id: '3', name: 'package.json' },
+  { id: '4', name: 'README.md' }
+]
 
 export default function Index() {
   const data = useLoaderData()
   const rowData = data.romdata
   return (
-    <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' }}>
-      <AgGridReact rowData={rowData} columnDefs={columnDefs} gridOptions={gridOptions}></AgGridReact>
+    <div className="container" style={{ height: '100vh', display: 'flex' }}>
+      <div className="tree-view" style={{ flexBasis: '33%', borderRight: '1px solid #ccc', padding: '10px' }}>
+        <Tree initialData={treeData} />
+      </div>
+      <div
+        className="ag-theme-alpine grid-view"
+        style={{ flexBasis: '66%', flexGrow: 1, height: '100%', width: '100%' }}
+      >
+        <AgGridReact rowData={rowData} columnDefs={columnDefs} gridOptions={gridOptions}></AgGridReact>
+      </div>
     </div>
   )
 }
