23dfdeffbeb616fe99ed9fbf52d0c4177cec5b23
tony woode <tonywoodecode@gmail.com>
2023-11-30 21:06:05 +0000

    getting closer to double click node icon/text...
    but the leaf never closes the parent

--- app/components/Node.tsx
+++ app/components/Node.tsx
@@ -11,24 +11,33 @@ const Node = ({ node, style, dragHandle, tree }) => {
   const targetURLStars = `/grid/${encodeURI(romdataStars)}`
   // console.log('currentURL', currentURLStars)
   // console.log('targetURL', targetURLStars)
+  const handleToggle = e => {
+    console.log('e', e)
+    e.preventDefault()
+    e.stopPropagation()
+    const nodeId = tree.mostRecentNode
+    // console.log('nodeId', nodeId)
+    return node.isInternal ? node.toggle() : tree.close(nodeId)
+  }
+  const handleDoubleClick = e => {
+    console.log('e', e)
+    e.preventDefault()
+    const nodeId = tree.mostRecentNode
+    // console.log('nodeId', nodeId)
+    return node.toggle()
+  }
   return (
     <div className="node-container" style={style} ref={dragHandle}>
       <div className="node-content" style={{ display: 'flex', alignItems: 'center' }}>
         <>
           {!node.isLeaf ? (
-            <span
-              className="arrow"
-              style={{ marginRight: '2px' }}
-              onClick={() => {
-                return node.isInternal && node.toggle()
-              }}
-            >
+            <span className="arrow" style={{ marginRight: '2px' }} onClick={handleToggle}>
               {node.isOpen ? <MdArrowDropDown /> : <MdArrowRight />}
             </span>
           ) : (
             ''
           )}
-          <span className="file-folder-icon" style={{ marginRight: '6px' }}>
+          <span className="file-folder-icon" style={{ marginRight: '6px' }} onDoubleClick={handleDoubleClick}>
             {node.data.iconLink ? (
               <img src={'/' + node.data.iconLink} alt="folder-icon" style={{ width: '32px', height: '32px' }} />
             ) : (
@@ -38,12 +47,12 @@ const Node = ({ node, style, dragHandle, tree }) => {
         </>
         {romdataStars && currentURLStars !== targetURLStars ? ( //don't navigate if we already clicked here (causes the tree to collapse)
           <Link to={targetURLStars}>
-            <span className="node-text">
+            <span className="node-text" onDoubleClick={handleDoubleClick}>
               <span>{node.data.name}</span>
             </span>
           </Link>
         ) : (
-          <span className="node-text">
+          <span className="node-text" onDoubleClick={handleDoubleClick}>
             <span>{node.data.name}</span>
           </span>
         )}
