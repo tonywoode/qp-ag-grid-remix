553b81911cbaf82579d9f0a5cc029e2a59a6b9ee
tony woode <tonywoodecode@gmail.com>
2023-12-04 21:36:54 +0000

    single+double click registering on icons...
    not correctly positioned, but shows the idea:
    https://medium.com/trabe/prevent-click-events-on-double-click-with-react-with-and-without-hooks-6bf3697abc40
    problem is hooking single click into Link component

--- app/components/Node.tsx
+++ app/components/Node.tsx
@@ -1,30 +1,42 @@
+import React from 'react'
 import { Link, useLocation } from '@remix-run/react'
 import { AiFillFolder, AiFillFile } from 'react-icons/ai'
 import { MdArrowRight, MdArrowDropDown } from 'react-icons/md'
+import useClickPreventionOnDoubleClick from '~/utils/doubleClick/use-click-prevention-on-double-click'
 
 export function Node({ node, style, dragHandle, tree }) {
+  const onClick = () => { console.log('you single clicked me'); return node.isInternal && node.toggle()} // prettier-ignore
+  const onDoubleClick = () => {console.log('you double clicked me'); return node.toggle() } //prettier-ignore
+  const [handleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(onClick, onDoubleClick)
   //a react-arborist node
   const location = useLocation()
   const romdataStars = node.data.romdataLink?.replace(/\//g, '*') //restore folder paths from url encoding
   const currentURLStars = location.pathname
   const targetURLStars = `/grid/${encodeURI(romdataStars)}`
-  const handleToggle = e => { e.stopPropagation();return node.isInternal && node.toggle()} // prettier-ignore
-  const handleDoubleClick = e => { e.preventDefault();return node.toggle() } //prettier-ignore
   const renderNodeText = () => (
     <span className="node-text" onDoubleClick={handleDoubleClick}>
       <span>{node.data.name}</span>
     </span>
   )
-
   return (
     <div className="node-container" style={style} ref={dragHandle}>
       <div className="node-content" style={{ display: 'flex', alignItems: 'center' }}>
         {!node.isLeaf && (
-          <span className="arrow" style={{ marginRight: '2px' }} onClick={handleToggle}>
+          <span
+            className="arrow"
+            style={{ marginRight: '2px' }}
+            onClick={handleClick}
+            onDoubleClick={handleDoubleClick}
+          >
             {node.isOpen ? <MdArrowDropDown /> : <MdArrowRight />}
           </span>
         )}
-        <span className="file-folder-icon" style={{ marginRight: '6px' }} onDoubleClick={handleDoubleClick}>
+        <span
+          className="file-folder-icon"
+          style={{ marginRight: '6px' }}
+          onClick={handleClick}
+          onDoubleClick={handleDoubleClick}
+        >
           {node.data.iconLink ? ( //do i need this anymore?
             <img src={node.data.icon} alt="folder-icon" style={{ width: '32px', height: '32px' }} />
           ) : (
