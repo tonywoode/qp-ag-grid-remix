c53aa0ff4205f08ade32432d40ddbbbfcc51815e
tony woode <tonywoodecode@gmail.com>
2023-12-05 21:52:34 +0000

    improvements to Node clicking....
    * cursor is a hand on romdata nodes
    * single-clicking non-romdata node opens children

--- app/components/Node.tsx
+++ app/components/Node.tsx
@@ -11,39 +11,40 @@ export function Node({ node, style, dragHandle }) {
   const currentURLStars = location.pathname
   const targetURLStars = `/grid/${encodeURI(romdataStars)}`
   const toggleNode = () => node.toggle()
-  const navigateToTarget = () => romdataStars && currentURLStars !== targetURLStars && navigate(targetURLStars)
+  const navigateToTarget = () => {
+    if (romdataStars && currentURLStars !== targetURLStars) {
+      navigate(targetURLStars)
+    } else {
+      node.toggle()
+    }
+  }
   const [handleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(toggleNode, toggleNode)
   const [handleLinkClick, handleLinkDoubleClick] = useClickPreventionOnDoubleClick(navigateToTarget, toggleNode)
-
-  return (
-    <div className="node-container" style={style} ref={dragHandle}>
-      <div className="node-content" style={{ display: 'flex', alignItems: 'center' }}>
-        {!node.isLeaf && (
-          <span
-            className="arrow"
-            style={{ marginRight: '2px' }}
-            onClick={handleClick}
-            onDoubleClick={handleDoubleClick}
-          >
-            {node.isOpen ? <MdArrowDropDown /> : <MdArrowRight />}
-          </span>
-        )}
-        <span
-          className="file-folder-icon"
-          style={{ marginRight: '6px' }}
-          onClick={handleLinkClick}
-          onDoubleClick={handleLinkDoubleClick}
-        >
-          {node.data.iconLink ? (
-            <img src={node.data.icon} alt="folder-icon" style={{ width: '32px', height: '32px' }} />
-          ) : (
-            <AiFillFolder color={node.isLeaf ? '#6bc7f6' : '#f6cf60'} />
-          )}
-        </span>
-        <span className="node-text" onClick={handleLinkClick} onDoubleClick={handleLinkDoubleClick}>
-          {node.data.name}
-        </span>
-      </div>
-    </div>
-  )
+ const cursorStyle = node.data.romdataLink ? { cursor: 'pointer' } : {}
+ return (
+   <div className="node-container" style={style} ref={dragHandle}>
+     <div className="node-content" style={{ display: 'flex', alignItems: 'center' }}>
+       {!node.isLeaf && (
+         <span className="arrow" style={{ marginRight: '2px' }} onClick={handleClick} onDoubleClick={handleDoubleClick}>
+           {node.isOpen ? <MdArrowDropDown /> : <MdArrowRight />}
+         </span>
+       )}
+       <span
+         className="file-folder-icon"
+         style={{ marginRight: '6px', ...cursorStyle }}
+         onClick={handleLinkClick}
+         onDoubleClick={handleLinkDoubleClick}
+       >
+         {node.data.iconLink ? (
+           <img src={node.data.icon} alt="folder-icon" style={{ width: '32px', height: '32px' }} />
+         ) : (
+           <AiFillFolder color={node.isLeaf ? '#6bc7f6' : '#f6cf60'} />
+         )}
+       </span>
+       <span className="node-text" style={cursorStyle} onClick={handleLinkClick} onDoubleClick={handleLinkDoubleClick}>
+         {node.data.name}
+       </span>
+     </div>
+   </div>
+ )
 }
