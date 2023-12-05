b7e4393347aef7cd1373b4ea44d8186028938c59
tony woode <tonywoodecode@gmail.com>
2023-12-05 21:07:22 +0000

    getting desired click link behaviour in Node...
    but why loading unrelated romdata twice then our romdata twice?

--- app/components/Node.tsx
+++ app/components/Node.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
-import { Link, useLocation } from '@remix-run/react'
+import { Link, useLocation, useNavigate } from '@remix-run/react'
 import { AiFillFolder, AiFillFile } from 'react-icons/ai'
 import { MdArrowRight, MdArrowDropDown } from 'react-icons/md'
 import useClickPreventionOnDoubleClick from '~/utils/doubleClick/use-click-prevention-on-double-click'
-
 export function Node({ node, style, dragHandle, tree }) {
   const onClick = () => { console.log('you single clicked me'); return node.isInternal && node.toggle()} // prettier-ignore
   const onDoubleClick = () => {console.log('you double clicked me'); return node.toggle() } //prettier-ignore
   const [handleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(onClick, onDoubleClick)
+
+  const navigate = useNavigate()
   //a react-arborist node
   const location = useLocation()
   const romdataStars = node.data.romdataLink?.replace(/\//g, '*') //restore folder paths from url encoding
   const currentURLStars = location.pathname
   const targetURLStars = `/grid/${encodeURI(romdataStars)}`
+  const onLinkClick = () => {
+    console.log('you single clicked me on a link')
+    return romdataStars && currentURLStars !== targetURLStars && navigate(targetURLStars)
+  }
+  const onLinkDoubleClick = () => {
+    console.log('you double clicked me on a link')
+    return node.toggle()
+  }
+  const [handleLinkClick, handleLinkDoubleClick] = useClickPreventionOnDoubleClick(onLinkClick, onLinkDoubleClick)
   const renderNodeText = () => (
-    <span className="node-text" onClick={handleClick} onDoubleClick={handleDoubleClick}>
+    <span className="node-text" onClick={handleLinkClick} onDoubleClick={handleLinkDoubleClick}>
       <span>{node.data.name}</span>
     </span>
   )
@@ -34,8 +44,8 @@ export function Node({ node, style, dragHandle, tree }) {
         <span
           className="file-folder-icon"
           style={{ marginRight: '6px' }}
-          onClick={handleClick}
-          onDoubleClick={handleDoubleClick}
+          onClick={handleLinkClick}
+          onDoubleClick={handleLinkDoubleClick}
         >
           {node.data.iconLink ? ( //do i need this anymore?
             <img src={node.data.icon} alt="folder-icon" style={{ width: '32px', height: '32px' }} />
@@ -44,7 +54,7 @@ export function Node({ node, style, dragHandle, tree }) {
           )}
         </span>
         {romdataStars && currentURLStars !== targetURLStars ? ( //don't navigate if we already clicked here (causes the tree to collapse)
-          <Link to={targetURLStars}>{renderNodeText()}</Link>
+          <span>{renderNodeText()} </span>
         ) : (
           renderNodeText()
         )}
