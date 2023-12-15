8a56282597453d37ee3e8c4fa0857b92add9e37d
tony woode <tonywoodecode@gmail.com>
2023-12-15 21:44:08 +0000

    extract out choosegoodmerge fn...
    * sadly not sufficient. Also does path.normalise really also need the replace?
    * added source emultors.ini files to example_inputs also

--- app/routes/runGame.tsx
+++ app/routes/runGame.tsx
@@ -19,7 +19,7 @@ export async function action({ request }: ActionArgs) {
     //TODO: should be an .env variable with a ui to set (or something on romdata conversation?)
     gamePath.replace(/^\{gamesDir\}/, macOSGamesDirPath)
   )
-  const gamePathMacOS = path.normalize(gamesDirReplacedPath)
+  const gamePathMacOS = path.normalize(gamesDirReplacedPath).replace(/\\/g, '/') //TODO: is the replace really required?
 
   // const gamePathMacOS = path.join(
   //   '/Volumes/Untitled/Games',
@@ -31,57 +31,67 @@ export async function action({ request }: ActionArgs) {
   createDirIfNotExist(tempDir)
   const outputDirectory = tempDir
 
-  if (defaultGoodMerge) {
-    //meaning row in the grid contains a pre-selected preferred rom to extract
-    await extractRom(gamePathMacOS, outputDirectory, defaultGoodMerge, logger)
-    const outputFile = path.join(outputDirectory, defaultGoodMerge)
-    runGame(outputFile)
+  const gameExtension = path.extname(gamePathMacOS)
+  if (gameExtension === '.7z') {
+    //TODO: sadly this isn't good enough, look at saturn games
+    await examine7z(gamePathMacOS, outputDirectory, defaultGoodMerge, emulatorName)
   } else {
-    console.log('listing archive', gamePathMacOS)
-    listArchive(gamePathMacOS) //todo: report progress - https://github.com/quentinrossetti/node-7z/issues/104
-      .progress(async (files: string[]) => {
-        const filenames = files.map(file => file.name)
-        logger.log(`fileOperations`, `7z listing: `, filenames)
-        const fallbackCountryCodes = new Map([
-          // would rather get these than wrong language TODO: think the fallback codes are Genesis-specific?
-          ['PD', 1],
-          ['Unl', 2],
-          ['Unk', 3]
-        ])
-        const countryCodePrefs = new Map([
-          ['B', 4],
-          ['A', 5],
-          ['4', 6],
-          ['U', 7],
-          ['W', 8], //world actually prob means there's only one country code in the rom?
-          ['E', 9],
-          ['UK', 10] //highest priority
-        ])
-        const countryCodes = new Map([...fallbackCountryCodes, ...countryCodePrefs])
-        const priorityCodes = new Map([
-          ['h', 1],
-          ['p', 2],
-          ['a', 3],
-          ['f', 4],
-          ['!', 5] //highest priority
-        ])
-        logger.log(
-          `goodMergeChoosing`,
-          `sending goodMerge country and priority choices to GoodMerge chooser`,
-          countryCodes,
-          priorityCodes
-        )
-        const pickedRom = chooseGoodMergeRom(filenames, countryCodes, priorityCodes, logger)
-        logger.log(`goodMergeChoosing`, `computer picked this rom:`, pickedRom)
+    await runGame(gamePathMacOS)
+  }
 
-        await extractRom(gamePathMacOS, outputDirectory, pickedRom, logger)
-        const outputFile = path.join(outputDirectory, pickedRom)
-        await runGame(outputFile)
+  async function examine7z(gamePathMacOS, outputDirectory, defaultGoodMerge, emulatorName) {
+    if (defaultGoodMerge) {
+      //meaning row in the grid contains a pre-selected preferred rom to extract
+      await extractRom(gamePathMacOS, outputDirectory, defaultGoodMerge, logger)
+      const outputFile = path.join(outputDirectory, defaultGoodMerge)
+      runGame(outputFile)
+    } else {
+      console.log('listing archive', gamePathMacOS)
+      listArchive(gamePathMacOS) //todo: report progress - https://github.com/quentinrossetti/node-7z/issues/104
+        .progress(async (files: string[]) => {
+          const filenames = files.map(file => file.name)
+          logger.log(`fileOperations`, `7z listing: `, filenames)
+          const fallbackCountryCodes = new Map([
+            // would rather get these than wrong language TODO: think the fallback codes are Genesis-specific?
+            ['PD', 1],
+            ['Unl', 2],
+            ['Unk', 3]
+          ])
+          const countryCodePrefs = new Map([
+            ['B', 4],
+            ['A', 5],
+            ['4', 6],
+            ['U', 7],
+            ['W', 8], //world actually prob means there's only one country code in the rom?
+            ['E', 9],
+            ['UK', 10] //highest priority
+          ])
+          const countryCodes = new Map([...fallbackCountryCodes, ...countryCodePrefs])
+          const priorityCodes = new Map([
+            ['h', 1],
+            ['p', 2],
+            ['a', 3],
+            ['f', 4],
+            ['!', 5] //highest priority
+          ])
+          logger.log(
+            `goodMergeChoosing`,
+            `sending goodMerge country and priority choices to GoodMerge chooser`,
+            countryCodes,
+            priorityCodes
+          )
+          const pickedRom = chooseGoodMergeRom(filenames, countryCodes, priorityCodes, logger)
+          logger.log(`goodMergeChoosing`, `computer picked this rom:`, pickedRom)
 
-        return files //this seems to have no effect see https://github.com/cujojs/when/blob/HEAD/docs/api.md#progress-events-are-deprecated
-      })
-      .then(archivePathsSpec => logger.log(`fileOperations`, `listed this archive: `, archivePathsSpec))
-      .catch(err => console.error('error listing archive: ', err))
+          await extractRom(gamePathMacOS, outputDirectory, pickedRom, logger)
+          const outputFile = path.join(outputDirectory, pickedRom)
+          await runGame(outputFile)
+
+          return files //this seems to have no effect see https://github.com/cujojs/when/blob/HEAD/docs/api.md#progress-events-are-deprecated
+        })
+        .then(archivePathsSpec => logger.log(`fileOperations`, `listed this archive: `, archivePathsSpec))
+        .catch(err => console.error('error listing archive: ', err))
+    }
   }
   return null
 
@@ -96,6 +106,7 @@ export async function action({ request }: ActionArgs) {
     if (matchedEmulator) {
       const retroarchCommandLine = extractRetroarchCommandLine(matchedEmulator)
       console.log('Retroarch Command Line:', retroarchCommandLine)
+      //sigh a problem with the data here: "emulatorName": "RetroArch Nintendo Gamecube Multiloader (Dolphin)",
       const retroarchExe = '/Applications/Retroarch.app/Contents/MacOS/RetroArch'
       const libretroCore = `/Users/twoode/Library/Application Support/RetroArch/${retroarchCommandLine}`
       const flagsToEmu = '-v -f'
@@ -120,7 +131,9 @@ export async function action({ request }: ActionArgs) {
     const libretroCoreMatch = parameters.match(/cores[^ ]+/)
 
     if (libretroCoreMatch) {
-      const libretroCorePath = libretroCoreMatch[0].replace(/\\/g, '/')
+      console.log('libretroCoreMatch', libretroCoreMatch)
+      const libretroCorePath = libretroCoreMatch[0].replace(/\\/g, '/').replace(/"/g, '') //TODO: try loading a GC game without the " removal!
+      console.log('libretroCorePath', libretroCorePath)
       return libretroCorePath.replace(/\.dll$/, '.dylib')
     } else {
       return 'No libretro core found in parameters string' //TODO: don't return a string as an error
