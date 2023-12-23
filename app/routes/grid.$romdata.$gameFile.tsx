5733a2f97a34f4301e40cc761178f08edcb758db
tony woode <tonywoodecode@gmail.com>
2023-12-23 11:18:59 +0000

    single click selects, double click lauches game...
    * use double-click util hook again
    * use ag-grid custom-cell renderer
    * includes commented attempts to get a delayed double-click to edit cell,
    however the behaviour i want is actually that a single click in the selected-row edits cell

---
+++ app/routes/grid.$romdata.$gameFile.tsx
@@ -0,0 +1,8 @@
+//remix loader
+export async function loader({ params }) {
+  return {}
+}
+
+export function gameFile() {
+  return <div>hello world </div>
+}
--- app/routes/grid.$romdata.tsx
+++ app/routes/grid.$romdata.tsx
@@ -3,8 +3,10 @@ import AgGridStyles from 'ag-grid-community/styles/ag-grid.css'
 import AgThemeAlpineStyles from 'ag-grid-community/styles/ag-theme-alpine.css'
 import type { CellClickedEvent } from 'ag-grid-community'
 //import { romdata } from '~/../data/Console/Nintendo 64/Goodmerge 3.21 RW/romdata.json' //note destructuring
-import { useLoaderData, useParams } from '@remix-run/react'
+import { Outlet, useLoaderData, useParams } from '@remix-run/react'
+import useClickPreventionOnDoubleClick from '~/utils/doubleClick/use-click-prevention-on-double-click'
 import { loadRomdata } from '~/load_romdata.server'
+import React from 'react'
 /** @type {(import('ag-grid-community').ColDef | import('ag-grid-community').ColGroupDef )[]} */
 
 export async function loader({ params }) {
@@ -13,7 +15,52 @@ export async function loader({ params }) {
   const romdata = romdataBlob.romdata
   return { romdata }
 }
+function CustomCellRenderer(props) {
+  // const [editing, setEditing] = React.useState(false)
+  // const [timer, setTimer] = React.useState(null)
 
+  const [handleSingleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(
+    () => {
+      console.log('single click')
+      // setTimer(
+      //   setTimeout(() => {
+      //     setEditing(true)
+      //   }, 500)
+      // ) // 500ms delay before entering edit mode
+    },
+    () => {
+      console.log('double click')
+      // if (timer) {
+      // clearTimeout(timer)
+      // setTimer(null)
+      // Handle short double-click here (launch game)
+      fetch('../runGame', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          gamePath: props.data.path,
+          defaultGoodMerge: props.data.defaultGoodMerge,
+          emulatorName: props.data.emulatorName
+        })
+      })
+    }
+  )
+  // React.useEffect(() => {
+  //   if (editing) {
+  //     props.api.startEditingCell({
+  //       rowIndex: props.node.rowIndex,
+  //       colKey: props.column.colId
+  //     })
+  //     setEditing(false)
+  //   }
+  // }, [editing, props.api, props.node.rowIndex, props.column.colId])
+
+  return (
+    <div onClick={handleSingleClick} onDoubleClick={handleDoubleClick}>
+      {props.value}
+    </div>
+  )
+}
 export default function Grid() {
   const data = useLoaderData()
   const params = useParams()
@@ -22,7 +69,9 @@ export default function Grid() {
   // for column definitions, get ALL keys from all objects, use a set and iterate, then map to ag-grid columnDef fields
   const columnDefs = [...new Set(rowData.flatMap(Object.keys))].map(field => {
     //remove the string {GamesDir}\ from the start of all path fields TODO: should have a lit button showing gameDir subsitiution is active
-    return field === 'path' ? { field, valueGetter: removeGamesDirPrefix } : { field }
+    return field === 'path'
+      ? { field, valueGetter: removeGamesDirPrefix, cellRenderer: 'customCellRenderer' }
+      : { field, cellRenderer: 'customCellRenderer' }
   })
   function removeGamesDirPrefix(params) {
     const originalValue = params.data.path // Assuming 'path' is the field in your data
@@ -33,25 +82,17 @@ export default function Grid() {
   console.table(columnDefs)
   /** @type {import('ag-grid-community').GridOptions} */
   const gridOptions = {
-    onCellClicked: (event: CellClickedEvent) => {
-      console.log(event.data)
-      fetch('../runGame', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          gamePath: event.data.path,
-          defaultGoodMerge: event.data.defaultGoodMerge,
-          emulatorName: event.data.emulatorName
-        })
-      })
-    },
+    components: { customCellRenderer: CustomCellRenderer },
     columnDefs: columnDefs,
-    defaultColDef: { flex: 1, minWidth: 150, editable: true }
+    defaultColDef: { flex: 1, minWidth: 150 }
   }
   return (
-    <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' }}>
-      <AgGridReact rowData={rowData} columnDefs={columnDefs} gridOptions={gridOptions}></AgGridReact>
-    </div>
+    <>
+      <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' }}>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs} gridOptions={gridOptions}></AgGridReact>
+      </div>
+      <Outlet />
+    </>
   )
 }
 
