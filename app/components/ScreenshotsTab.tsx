840f3c369cf7e7c7b356efd769b6ffb81fd43ae4
tony woode <tonywoodecode@gmail.com>
2024-01-04 20:05:26 +0000

    [tabs] screenshots take up full dimensions

--- app/components/ScreenshotsTab.tsx
+++ app/components/ScreenshotsTab.tsx
@@ -2,7 +2,7 @@ export function ScreenshotsTab({ screenshots }) {
   return (
     <div>
       {screenshots.map((screenshot, index) => (
-        <img key={index} src={screenshot} alt={`Screenshot ${index}`} />
+        <img key={index} src={screenshot} alt={`Screenshot ${index}`} style={{ width: '100%', height: 'auto' }} />
       ))}
     </div>
   )
