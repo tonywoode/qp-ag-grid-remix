0355d78f1315371ec6680851245ac07583f2dc81
tony woode <tonywoodecode@gmail.com>
2024-05-25 19:13:26 +0100

    [grid] run game with enter key

--- app/routes/grid.$romdata.tsx
+++ app/routes/grid.$romdata.tsx
@@ -1,7 +1,7 @@
 import { AgGridReact } from 'ag-grid-react'
 import AgGridStyles from 'ag-grid-community/styles/ag-grid.css'
 import AgThemeAlpineStyles from 'ag-grid-community/styles/ag-theme-alpine.css'
-import type { CellClickedEvent, GridOptions, ColDef, ColGroupDef } from 'ag-grid-community'
+import type { CellKeyDownEvent, CellClickedEvent, GridOptions, ColDef, ColGroupDef } from 'ag-grid-community'
 //import { romdata } from '~/../data/Console/Nintendo 64/Goodmerge 3.21 RW/romdata.json' //note destructuring
 import { Outlet, useLoaderData, useParams, useNavigate } from '@remix-run/react'
 import useClickPreventionOnDoubleClick from '~/utils/doubleClick/use-click-prevention-on-double-click'
@@ -16,6 +16,13 @@ export async function loader({ params }) {
   // let { screenshots } = await loadScreenshots()
   const romdataBlob = await loadRomdata(romdataLink)
   return { romdata: romdataBlob.romdata }
+}
+
+const runGameWithRomdataFromEvent = (e: CellClickedEvent) => {
+  const {
+    data: { path, defaultGoodMerge, emulatorName }
+  } = e
+  runGame({ gamePath: path, defaultGoodMerge, emulatorName })
 }
 
 export const runGame = gameData => {
@@ -66,17 +73,16 @@ export default function Grid() {
   const navigate = useNavigate()
   const [clickedCell, setClickedCell] = useState(null)
   const [handleSingleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(
-    async e => {
+    async (e: CellClickedEvent) => {
       console.log('single click')
       console.log(e.data)
       const { node: { rowIndex }, column: { colId }, api }: CellClickedEvent = e // prettier-ignore
       setClickedCell({ rowIndex, colKey: colId })
       api.startEditingCell({ rowIndex, colKey: colId })
     },
-    e => {
+    (e: CellClickedEvent) => {
       console.log('double click')
-      const { data: { path, defaultGoodMerge, emulatorName } }: CellClickedEvent = e //prettier-ignore
-      runGame({ gamePath: path, defaultGoodMerge, emulatorName })
+      runGameWithRomdataFromEvent(e)
     }
   )
 
@@ -106,17 +112,19 @@ export default function Grid() {
     suppressClickEdit: true,
     onCellClicked: handleSingleClick,
     onCellDoubleClicked: handleDoubleClick,
-    onCellKeyDown: function (event) {
-      console.log('event.key is ' + event.event.key)
+    onCellKeyDown: function (e: CellKeyDownEvent) {
+      console.log('event.key is ' + e.event.key)
       //don't multiple select when arrow keys used for navigation
-      if (event.event.key === 'ArrowUp' || event.event.key === 'ArrowDown') {
-        const focusedNode = event.api.getDisplayedRowAtIndex(event.api.getFocusedCell().rowIndex)
-        event.api.forEachNode(node => {
+      if (e.event.key === 'ArrowUp' || e.event.key === 'ArrowDown') {
+        const focusedNode = e.api.getDisplayedRowAtIndex(e.api.getFocusedCell().rowIndex)
+        e.api.forEachNode(node => {
           if (node !== focusedNode) {
             node.setSelected(false)
           }
         })
         focusedNode.setSelected(true)
+      } else if (e.event.key === 'Enter') {
+        runGameWithRomdataFromEvent(e)
       }
     },
     onRowSelected: async function (event) {
