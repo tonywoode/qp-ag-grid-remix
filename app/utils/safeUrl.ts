8be2a4a7259fa97cb6a5f9950fdcfc30b254e6c3
tony woode <tonywoodecode@gmail.com>
2024-06-13 19:47:33 +0100

    [tree] fix single click...
    encode/decode uri is often null, don't try to split on it if so

--- app/utils/safeUrl.ts
+++ app/utils/safeUrl.ts
@@ -1,9 +1,9 @@
 export function encodeString(str: string) {
-  let replacedString = str.replace(/\\/g, '~') // replace backslashes with ~
+  let replacedString = str?.replace(/\\/g, '~') // replace backslashes with ~
   let encodedString = encodeURIComponent(replacedString)
   return encodedString
 }
 export function decodeString(str: string) {
-  let decodedString = decodeURIComponent(str).replace(/~/g, '\\')
+  let decodedString = decodeURIComponent(str)?.replace(/~/g, '\\')
   return decodedString
 }
