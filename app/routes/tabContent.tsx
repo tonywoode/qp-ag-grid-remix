f105053ae55be230b9f104d1e4bfad49076805ec
tony woode <tonywoodecode@gmail.com>
2024-07-24 18:56:18 +0100

    [media] remove unnecessary in tabContent route

--- app/routes/tabContent.tsx
+++ app/routes/tabContent.tsx
@@ -5,33 +5,14 @@ export async function action({ request }: ActionFunctionArgs) {
   const { tabClass, searchType, romname, selectedTab, system, mameNames, mameUseParentForSrch } = await request.json()
   console.log('in the tabData action function')
   console.log(tabClass, romname, selectedTab, system)
-  if (tabClass === 'screenshot') {
-    const { screenshots } = await getTabContent(
-      tabClass,
-      searchType,
-      romname,
-      selectedTab,
-      system,
-      mameNames,
-      mameUseParentForSrch
-    )
-    console.log('screenshots')
-    console.log(screenshots)
-    return { screenshots }
-  } else if (tabClass === 'mameDat') {
-    const { mameDat } = await getTabContent(
-      tabClass,
-      searchType,
-      romname,
-      selectedTab,
-      system,
-      mameNames,
-      mameUseParentForSrch
-    )
-    // console.log('mameDat')
-    // console.log(mameDat)
-    return { mameDat }
-  } else {
-    return {}
-  }
+  const tabContent = await getTabContent(
+    tabClass,
+    searchType,
+    romname,
+    selectedTab,
+    system,
+    mameNames,
+    mameUseParentForSrch
+  )
+  return tabContent ? tabContent : {}
 }
