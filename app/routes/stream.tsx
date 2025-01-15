fc7dcea37bc620904255edd7fd071f38652922f7
tony woode <tonywoodecode@gmail.com>
2025-01-15 23:26:54 +0000

    [SSSE] remade stream.tsx...
    compare with sergios version, i was fed up with the constant [ERR_INVALID_STATE]: Invalid state: Controller is already closed upon causing a frontend reload

--- app/routes/stream.tsx
+++ app/routes/stream.tsx
@@ -1,16 +1,27 @@
-import type { LoaderFunctionArgs } from '@remix-run/node'
-import { eventStream } from 'remix-utils/sse/server'
 import { emitter } from '~/utils/emitter.server'
+import { LoaderFunctionArgs } from '@remix-run/node'
+import { PassThrough } from 'stream'
+//import { eventStream } from 'remix-utils/sse/server' - using sergios version caused a crash whenever the frontend changed ie: a refresh - "invalid state: Controller is already closed"
 
-export function loader({ request }: LoaderFunctionArgs) {
-  return eventStream(request.signal, function setup(send) {
-    function handle(data: any) {
-      console.log('runGame event:', data)
-      send({ event: 'runGameEvent', data: JSON.stringify(data) })
-    }
-    emitter.on('runGameEvent', handle)
-    return function cleanup() {
-      emitter.off('runGameEvent', handle)
+export async function loader({ request }: LoaderFunctionArgs) {
+  let responseStream = new PassThrough()
+  function sendEvent(eventObj: any) {
+    responseStream.write(`event: runGameEvent\ndata: ${JSON.stringify(eventObj)}\n\n`)
+  }
+  sendEvent({ type: 'connected', data: new Date().toISOString() })
+  emitter.on('runGameEvent', data => {
+    sendEvent(data)
+  })
+  request.signal.addEventListener('abort', () => {
+    emitter.removeAllListeners('runGameEvent')
+    responseStream.end()
+  })
+  return new Response(responseStream, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache, no-transform', //TODO: sergio didn't send no-transform
+      Connection: 'keep-alive'
     }
   })
 }
--- app/utils/emitter.server.ts
+++ app/utils/emitter.server.ts
@@ -1,4 +1,3 @@
 import { EventEmitter } from 'events'
-const emitter = new EventEmitter()
+export const emitter = new EventEmitter()
 //using kcd's remember causes complaints about controller already being closed...
-export { emitter }
