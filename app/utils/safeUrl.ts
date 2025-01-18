5c54507d50ca316f24c86479e89db19826a8e55c
tony woode <tonywoodecode@gmail.com>
2025-01-18 20:51:52 +0000

    make url encode/decode safer...
    with commented base64 enc/dec if that's still not enough

--- app/utils/safeUrl.ts
+++ app/utils/safeUrl.ts
@@ -1,9 +1,34 @@
-export function encodeString(str: string) {
-  let replacedString = str?.replace(/\\/g, '~') // replace backslashes with ~
-  let encodedString = encodeURIComponent(replacedString)
-  return encodedString
+const SPECIAL_CHARS_MAP = {
+  '\\': '__BSLASH__',
+  '%': '__PERCENT__',
+  '~': '__TILDE__',
+  '#': '__HASH__',
+  '?': '__QMARK__',
+  '/': '__FSLASH__',
+  ':': '__COLON__'
 }
+
+export function encodeString(str: string) {
+  if (!str) return ''
+
+  // Replace special characters with safe tokens
+  const safeString = str.replace(/[\\%~#?/:]/g, char => SPECIAL_CHARS_MAP[char])
+
+  // could convert to Base64 encode as final safety net, but its nice to read the url
+  // return btoa(safeString)
+  return safeString
+}
+
 export function decodeString(str: string) {
-  let decodedString = decodeURIComponent(str)?.replace(/~/g, '\\')
-  return decodedString
+  if (!str) return ''
+
+  // Decode Base64
+  // const decoded = atob(str)
+
+  // Replace tokens back with special characters
+  //return decoded.replace(
+  return str.replace(
+    /__BSLASH__|__PERCENT__|__TILDE__|__HASH__|__QMARK__|__FSLASH__|__COLON__/g,
+    match => Object.entries(SPECIAL_CHARS_MAP).find(([_, v]) => v === match)?.[0] || match
+  )
 }
