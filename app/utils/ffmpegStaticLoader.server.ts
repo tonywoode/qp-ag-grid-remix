77ffd1d62653b1cb8a35c8b15d0423fd4880693f
tony woode <tonywoodecode@gmail.com>
2025-02-16 16:49:21 +0000

    even better ffmpeg path

--- app/utils/ffmpegStaticLoader.server.ts
+++ app/utils/ffmpegStaticLoader.server.ts
@@ -4,18 +4,22 @@ import { pathToFileURL } from 'url'
 
 export function loadFfmpegPath() {
   let unpackedPath
+  // if (electron.app.isPackaged) {
+  //   const ffmpegPath = join(
+  //     process.resourcesPath,
+  //     'app.asar.unpacked',
+  //     'node_modules',
+  //     'ffmpeg-static',
+  //     process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
+  //   )
+  //   unpackedPath = pathToFileURL(ffmpegPath).toString()
+  // } else {
+  //   unpackedPath = require('ffmpeg-static')
+  // }
   if (electron.app.isPackaged) {
-    const ffmpegPath = join(
-      process.resourcesPath,
-      'app.asar.unpacked',
-      'node_modules',
-      'ffmpeg-static',
-      process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
-    )
-    unpackedPath = pathToFileURL(ffmpegPath).toString()
+    unpackedPath = require('ffmpeg-static').replace('app.asar', 'app.asar.unpacked')
   } else {
     unpackedPath = require('ffmpeg-static')
   }
-
   return unpackedPath
 }
