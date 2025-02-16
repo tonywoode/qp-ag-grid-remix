95c8ca225c9b57e3b5f015497a2dbb8abc3b4cbd
tony woode <tonywoodecode@gmail.com>
2025-02-16 16:01:40 +0000

    ffmpeg path

--- app/utils/ffmpegStaticLoader.server.ts
+++ app/utils/ffmpegStaticLoader.server.ts
@@ -3,8 +3,9 @@ import electron from '~/electron.server'
 import { pathToFileURL } from 'url'
 
 export function loadFfmpegPath() {
+  //TODO: only include .exe if we're on windows!
   let unpackedPath = electron.app.isPackaged
-    ? join(process.resourcesPath, 'app.asar.unpacked', 'node_modules', 'ffmpeg-static', 'ffmpeg')
+    ? join(process.resourcesPath, 'app.asar.unpacked', 'node_modules', 'ffmpeg-static', 'ffmpeg.exe')
     : require('ffmpeg-static')
   if (electron.app.isPackaged) {
     // 'convert path to URL for ESM loader
